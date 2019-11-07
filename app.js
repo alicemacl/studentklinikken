c548bbc20700674c2cfea36813f4f331cbf45add
Mats Hagen <mhagen@Matss-MacBook-Pro.local>
2019-11-07 15:18:58 +0100

    added admin.html

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import router from "./router.js";
 import * as components from "./components/index.js";
 
-new Vue ({ 
-    el: "#app", 
+new Vue({
+    el: "#app",
+    router: router
+})
+
+new Vue({
+    el: "#admin",
     router: router
 })
