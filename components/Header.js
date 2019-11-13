1bd0fd47c6b36ac69685a03dd0401b3e738c6d37
Alice <alice.clausen@gmail.com>
2019-11-13 12:44:49 +0100

    header changes

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,18 @@
 const Header = Vue.component("header-component", {
   template: `
-  <div class="header">
-    <div class="row" id="menu-row-spacing"></div> 
-    <div class="row" id="menu-row-content"> 
-      <div class="col-sm-3"></div>
-      <div class="col-sm-6 d-flex justify-content-center align-items-center">
-          <router-link to='/'><img class="logo" src="../assets/Logo-farger.png"></router-link>
-      </div>
-      <div class="col-sm-3 d-flex justify-content-center align-items-center">
-          <button @click="toggle" >
-              <img class="button" src="../assets/menu-button.png">
-          </button>
-      </div>
-  </div>
-  <div class="row" id="menu-row-spacing"></div> 
+    <div class="header">
+        <div class="row header-flex">
+            <div class="empty"></div>
+            <div class="header-logo">
+                <router-link to='/'><img class="logo" src="../assets/Logo-farger.png"></router-link>
+            </div>
+            <div class="menu-btn">
+                <button @click="toggle">
+                    <img class="button" src="../assets/menu-button.png">
+                </button>
+            </div>
+        </div>
+    </div>
     <!-- <menu-component @close="toggle" :closable="true"></menu-component> -->
         <Menu @close="toggle" :align="align" :closable="true">
             <div v-if="open">
