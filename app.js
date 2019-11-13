73ef41af036562ad820a25c453b2986337452130
Mats Hagen <mhagen@Matss-MacBook-Pro.local>
2019-11-13 13:57:00 +0100

    Added Admin pages structure and components files

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import router from "./router.js";
 import * as components from "./components/index.js";
 
-<<<<<<< HEAD
-new Vue({
-    el: "#app",
-    router: router
-})
-=======
 new Vue ({ 
     el: "#app", 
     router: router,
 });
->>>>>>> bd614c904dff417fe6e42981ae74ed919304ec14
--- router.js
+++ router.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import Home from "./pages/Home.js"
-import Behandlinger from "./pages/Behandlinger.js"
-import AnsatteHjem from "./pages/AnsatteHjem.js"
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/behandlinger',
-            name: 'behandlinger',
-            component: Behandlinger
-        },
-        {
-            path: '/ansatte',
-            name: 'ansatte',
-            component: AnsatteHjem
-        }
-    ]
-=======
 import Home from "./pages/Home.js";
 import Behandlinger from "./pages/Behandlinger.js";
 import Osteopati from "./pages/Osteopati.js"; 
@@ -49,7 +24,6 @@ const router = new VueRouter({
     }
   ],
 
->>>>>>> bd614c904dff417fe6e42981ae74ed919304ec14
 });
 
 export default router;
