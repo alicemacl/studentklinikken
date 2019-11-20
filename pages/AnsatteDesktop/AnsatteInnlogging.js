c17060aeb9818a8b91355fa89c91154b48d43c35
evyjeanett <evyjha@gmail.com>
2019-11-20 09:21:15 +0100

    Made changes to ansatteInnlogging

--- pages/AnsatteDesktop/AnsatteInnlogging.js
+++ pages/AnsatteDesktop/AnsatteInnlogging.js
@@ -2,30 +2,30 @@ const AnsatteInnlogging = {
     template: `
     <div>
         <div class="innlogging-ansatte d-flex flex-column justify-content-center">
-            <img class="logo" src="../assets/logoSort.png">
-            <div class="">
+            <img class="logo align-self-center" src="../assets/logoSort.png">
+            <div class=" d-flex justify-content-center">
                     <h1 class="main-header">Innlogging ansatte</h1>
             </div>
-            <div class="">
-                <input type="text" class="input" placeholder="E-post/telefonnummer"><br>
-                <input type="text" class="input" placeholder="Passord"><br>
-                <button class="btn action-btn btn-primary logg-inn-btn">Logg inn</button>
+            <div class="d-flex flex-column">
+                <input type="text" class="input align-self-center" placeholder="E-post/telefonnummer">
+                <input type="text" class="input align-self-center" placeholder="Passord">
+                <button class="btn action-btn btn-primary logg-inn-btn align-self-center">Logg inn</button>
             </div>
 
             <div class="">
-                <div class="">
-                    <input type="checkbox"> Husk meg<br>
+                <div class="d-flex justify-content-center">
+                    <input class="align-self-center" type="checkbox"> Husk meg
                 </div>
-                <div class="">
+                <div class="d-flex justify-content-center">
                     <router-link to="/password">
-                        <a href="">Glemt passord?</a>
+                        <h3>Glemt passord?</h3>
                     </router-link>
                 </div>     
             </div>
                 
-            <div class="">
-                    <router-link to="/home">
-                        <a href=""><-- Tilbake til studentklinikken</a>
+            <div class="d-flex justify-content-center">
+                    <router-link to="/">
+                        <h3><img class="arrow" src="../../assets/arrow-right.png"> Tilbake til studentklinikken </h3>
                     </router-link>
                 </div>
         </div>
